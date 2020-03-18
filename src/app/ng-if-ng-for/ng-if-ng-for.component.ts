7111814b06043dfb1f82ee1e3c0f4727210b90e5
pedroBotega99 <pedro.botega@fingerdigital.com.br>
2020-03-18 15:25:40 -0300

    teste de ngIf e ngFor

--- src/app/ng-if-ng-for/ng-if-ng-for.component.ts
+++ src/app/ng-if-ng-for/ng-if-ng-for.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-ng-if-ng-for',
-  templateUrl: './ng-if-ng-for.component.html',
-  styleUrls: ['./ng-if-ng-for.component.scss']
+	selector: 'app-ng-if-ng-for',
+	templateUrl: './ng-if-ng-for.component.html',
+	styleUrls: ['./ng-if-ng-for.component.scss']
 })
 export class NgIfNgForComponent implements OnInit {
+	arrNumeros = ['pedro', 'de', 'souza', 'botega'];
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
